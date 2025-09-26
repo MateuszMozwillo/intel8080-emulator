84ead79f30ced5ae77036dfbfe99d9fc514e7895
MateuszMozwillo <109803607+MateuszMozwillo@users.noreply.github.com>
2025-09-26 21:37:18 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 
 #define HALT 0b01110110
 
+// sources: http://dunfield.classiccmp.org/r/8080.txt, https://pastraiser.com/cpu/i8080/i8080_opcodes.html
+
 typedef enum {
     REG_B,
     REG_C,
@@ -33,12 +35,16 @@ typedef enum {
     CC_M
 } ConditionCode;
 
-static inline Register extract_dst_reg(uint8_t instruction) {
-    return (Register)((instruction >> 3) & 0x07);
+static inline Register extract_dst_reg(uint8_t opcode) {
+    return (Register)((opcode >> 3) & 0x07);
 }
 
-static inline Register extract_src_reg(uint8_t instruction) {
-    return (Register)(instruction & 0x07);
+static inline Register extract_src_reg(uint8_t opcode) {
+    return (Register)(opcode & 0x07);
+}
+
+static inline RegisterPair extract_reg_pair(uint8_t opcode) {
+    return (RegisterPair)((opcode >> 4) & 0x03);
 }
 
 typedef struct {
@@ -55,6 +61,19 @@ typedef struct {
 
 static inline uint16_t cpu_get_hl(CpuState *cpu) {
     return (((uint16_t)cpu->h << 8) | cpu->l);
+}
+
+static inline uint16_t lb_hb_to_uint16(uint8_t low_byte, uint8_t high_byte) {
+    return ((uint16_t)high_byte << 8) | low_byte;
+}
+
+static inline void cpu_set_reg_pair(CpuState *cpu, RegisterPair rp, uint8_t low_byte, uint8_t high_byte) {
+    switch(rp) {
+        case REG_P_BC: cpu->b = high_byte; cpu->c = low_byte; break;
+        case REG_P_DE: cpu->d = high_byte; cpu->e = low_byte; break;
+        case REG_P_HL: cpu->h = high_byte; cpu->l = low_byte; break;
+        case REG_P_SP: cpu->sp = lb_hb_to_uint16(low_byte, high_byte); break;
+    }
 }
 
 static inline uint8_t cpu_read_reg(CpuState *cpu, Register r) {
@@ -83,25 +102,62 @@ static inline void cpu_set_reg(CpuState *cpu, Register r, uint8_t val) {
     }
 }
 
-// MOV 01DDDSSS         (moves DDD reg to SSS reg)
+// returns byte from memory location at program counter + offset
+static inline uint8_t read_byte(CpuState *cpu, uint8_t pc_offset) {
+    return cpu->mem[cpu->pc+pc_offset];
+}
+
+// MOV  01DDDSSS         (moves DDD reg to SSS reg)
 static inline void cpu_mov(CpuState *cpu, uint8_t opcode) {
     Register dst = extract_dst_reg(opcode);
     Register src = extract_src_reg(opcode);
     cpu_set_reg(cpu, dst, cpu_read_reg(cpu, src));
-    cpu->pc++;
+    cpu->pc += 1;
 }
 
-// MVI 00DDD110 db      (moves immediate to DDD reg)
+// MVI  00DDD110 db      (moves immediate to DDD reg)
 static inline void cpu_mvi(CpuState *cpu, uint8_t opcode) {
     Register dst = extract_dst_reg(opcode);
-    uint8_t immediate = cpu->mem[cpu->pc+1];
+    uint8_t immediate = read_byte(cpu, 1);
     cpu_set_reg(cpu, dst, immediate);
     cpu->pc += 2;
 }
 
-// LXI 00RP0001 lb hb   (loads 16 bit immediate to register pair)
+// LXI  00RP0001 lb hb   (loads 16 bit immediate to register pair)
 static inline void cpu_lxi(CpuState *cpu, uint8_t opcode) {
+    RegisterPair dst = extract_reg_pair(opcode);
+    cpu_set_reg_pair(cpu, dst, read_byte(cpu, 1), read_byte(cpu, 2));
+    cpu->pc += 3;
+}
 
+// LDA  00111010 lb hb   (loads data from address to reg A)
+static inline void cpu_lda(CpuState *cpu, uint8_t opcode) {
+    cpu->a = cpu->mem[lb_hb_to_uint16(read_byte(cpu, 1), read_byte(cpu, 2))];
+    cpu->pc += 3;
+}
+
+// STA  00110010 lb hb   (stores reg A to address)
+static inline void cpu_sta(CpuState *cpu, uint8_t opcode) {
+    cpu->mem[lb_hb_to_uint16(read_byte(cpu, 1), read_byte(cpu, 2))] = cpu->a;
+    cpu->pc += 3;
+}
+
+// LHLD 00101010 lb hb   (load hl pair from mem)
+static inline void cpu_lhld(CpuState *cpu, uint8_t opcode) {
+    cpu_set_reg_pair(cpu, REG_P_HL, read_byte(cpu, 1), read_byte(cpu, 2));
+    cpu->pc += 3;
+}
+
+// SHLD 00100010 lb hb   (stores hl to mem)
+static inline void cpu_shld(CpuState *cpu, uint8_t opcode) {
+    cpu->mem[lb_hb_to_uint16(read_byte(cpu, 1), read_byte(cpu, 2))] = cpu_read_reg(cpu, REG_L);
+    cpu->mem[lb_hb_to_uint16(read_byte(cpu, 1), read_byte(cpu, 2))+1] = cpu_read_reg(cpu, REG_H);
+    cpu->pc += 3;
+}
+
+// LDAX 00RP1010         (loads value from address from RP to A reg only BC or DE)
+static inline void cpu_ldax(CpuState *cpu, uint8_t opcode) {
+    
 }
 
 int main() {
